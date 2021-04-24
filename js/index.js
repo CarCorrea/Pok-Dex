e871c4d40d56a2476eb5d6b6d31b37cd5a691033
Carlos Correa <carlos.correa.zapata@gmail.com>
2021-04-23 21:09:59 -0400

    Generar tabla de pokemones

--- js/index.js
+++ js/index.js
@@ -17,6 +17,45 @@ tinymce.init({
   
 const pokemones =[]; 
 
+const cargarTabla = () =>{
+  //1.- Referencia a la tabla
+  let tbody = document.querySelector("#tbody-pokemon");  
+  //Antes del for debo limpiar la tabla
+  tbody.innerHTML = "";
+  //2.- Por cada pokémon generar una fila
+  for(let i = 0 ; i < pokemones.length ; i++) {
+    let p = pokemones[i];
+    // Crea un elemento que no existe (tr) pero no lo agrega a la página
+    let tr = document.createElement("tr");
+    //3.- Por cada atributo de pokemon, generar una zelda (td)
+    let tdNombre = document.createElement("td");
+    let tdTipo = document.createElement("td");
+    let tdDescripcion = document.createElement("td");
+    let tdNro = document.createElement("td");
+    let tdAcciones = document.createElement("td");
+
+    tdNombre.innerText = p.nombre;
+    tdTipo.innerText = p.tipo;    
+    tdDescripcion.innerHTML = p.descripcion;
+    tdNro.innerText = i + 1;
+    //tdAcciones.innerText = 
+    //TO DO: Como agrego un boton para las acciones
+    tr.appendChild(tdNro);
+    tr.appendChild(tdNombre);
+    tr.appendChild(tdTipo);
+    tr.appendChild(tdDescripcion);
+    tr.appendChild(tdAcciones);
+    tbody.appendChild(tr);
+    
+  };
+  
+  //4.- Agregar esa fila a la tabla
+
+  
+
+
+};
+
 document.querySelector("#pokemon-form").addEventListener('submit', (e)=>{
     e.preventDefault(); // Prevenir que el formulario recargue la pag
     let nombre = document.querySelector("#nombre-txt").value;
@@ -33,8 +72,9 @@ document.querySelector("#pokemon-form").addEventListener('submit', (e)=>{
         pokemon.tipo = tipo;
 
     pokemones.push(pokemon);
-    console.log(pokemones);
+    cargarTabla();
     
+    Swal.fire("Pokémon Registrado");
     });
 
 
